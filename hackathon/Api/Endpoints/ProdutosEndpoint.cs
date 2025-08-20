f14d0563ea3e988d206115612b35113ec5f359ac
Enzo Suzart <suzart_@outlook.com>
2025-08-19 22:45:09 -0300

    altera nomes dos endpoints

--- hackathon/Api/Endpoints/ProdutosEndpoint.cs
+++ hackathon/Api/Endpoints/ProdutosEndpoint.cs
@@ -7,7 +7,7 @@ public static class ProdutosEndpoint
 {
     public static void MapProdutos(this WebApplication app)
     {
-        app.MapGet("/produtos/volume-diario", async (
+        app.MapGet("/Volume", async (
             [FromQuery] DateOnly dataReferencia, // O RDG sabe como ler isso da querystring
             IObterVolumeDiarioUseCase useCase   // O RDG pede ao DI para injetar o use case
         ) =>
--- hackathon/Api/Endpoints/SimulacoesEndpoint.cs
+++ hackathon/Api/Endpoints/SimulacoesEndpoint.cs
@@ -8,13 +8,13 @@ public static class SimulacoesEndpoint
 {
     public static void MapSimulacoes(this WebApplication app)
     {
-        app.MapPost("/simulacoes", async (SimulacaoRequest request, ISimularEmprestimoUseCase useCase) =>
+        app.MapPost("/Simulacao", async (SimulacaoRequest request, ISimularEmprestimoUseCase useCase) =>
         {
             var resultado = await useCase.ExecutarAsync(request);
             return resultado is null ? Results.NotFound("Nenhum produto compatível encontrado.") : Results.Ok(resultado);
         });
 
-        app.MapGet("/simulacoes", async ([AsParameters] ListarRequest request, IObterSimulacoesUseCase useCase) =>
+        app.MapGet("/Simulacao", async ([AsParameters] ListarRequest request, IObterSimulacoesUseCase useCase) =>
         {
             var resultado = await useCase.ExecutarAsync(request);
             return Results.Ok(resultado);
